144ccc5f648e6abf8df780d189bd4daf8be391b0
Zedjones <dojoman19@gmail.com>
2019-12-18 17:01:30 -0500

    Added some test code

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+
+using PokeApiNet.Models;
 
 namespace PokePredict.Controllers
 {
@@ -11,6 +15,7 @@ namespace PokePredict.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        PokeApiNet.PokeApiClient client = new PokeApiNet.PokeApiClient();
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -34,6 +39,37 @@ namespace PokePredict.Controllers
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+                [HttpGet("/test")]
+        public IEnumerable<int> Test()
+        {
+            var myStack = new ConcurrentStack<int>();
+            Parallel.For(0, 100, i =>
+            {
+                myStack.Push(i);
+            });
+            return myStack.ToArray();
+        }
+        [HttpGet("/pokemon")]
+        public async Task<IActionResult> Pokemon()
+        {
+            var mon = Request.Query["pokemon"];
+            if (mon.Count == 0)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                var fullmon = await client.GetResourceAsync<Pokemon>(mon);
+                var allMoves = await client.GetResourceAsync(
+                    fullmon.Moves.Select(move => move.Move)
+                );
+                return Ok(allMoves);
+            }
+            catch (HttpRequestException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
