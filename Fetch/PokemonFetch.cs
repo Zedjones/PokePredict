ab197a88a0af26b3604c4692337dae21eed0ed78
Zedjones <dojoman19@gmail.com>
2019-12-26 22:21:24 -0500

    Added check for number of files/types before downloading

--- Fetch/PokemonFetch.cs
+++ Fetch/PokemonFetch.cs
@@ -1,13 +1,17 @@
 using PokeApiNet.Models;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.IO;
 
 namespace PokePredict.Fetch
 {
     public class PokemonFetch
     {
         private static async Task CacheAllPokemon(PokeApiNet.PokeApiClient client, string previousPath) {
-            var allMons = await client.GetNamedResourcePageAsync<Pokemon>(1000, 0);
+            var allMons = await client.GetNamedResourcePageAsync<Pokemon>(int.MaxValue, 0);
+            var pokePath = Path.Join(previousPath, "Pokemon");
+            // If we already have all the Pokemon, return
+            if(new DirectoryInfo(pokePath).GetFiles().Length == allMons.Count) return;
             var detailedMons = await client.GetResourceAsync(allMons.Results);
             Parallel.ForEach(detailedMons, myMons => {
                 var fullMon = new PokePredict.Database.Models.Pokemon(myMons, previousPath);
@@ -17,17 +21,20 @@ namespace PokePredict.Fetch
         }
         private static async Task CacheAllTypes(PokeApiNet.PokeApiClient client, string previousPath) {
             var types = await client.GetNamedResourcePageAsync<Type>(int.MaxValue, 0);
+            var typePath = Path.Join(previousPath, "Types");
+            // If we already have all the types, return
+            if(new DirectoryInfo(typePath).GetFiles().Length == types.Count) return;
             var allTypes = await client.GetResourceAsync(types.Results);
             Parallel.ForEach(allTypes, myType => {
                 var fullType = new PokePredict.Database.Models.Type(myType, previousPath);
                 fullType.WriteOut();
             });
         }
-        public static void CacheAll(string previousPath) {
+        public static void CacheAll(string basePath) {
             var client = new PokeApiNet.PokeApiClient();
             var tasks = new List<Task>();
-            tasks.Add(PokemonFetch.CacheAllTypes(client, previousPath));
-            tasks.Add(PokemonFetch.CacheAllPokemon(client, previousPath));
+            tasks.Add(PokemonFetch.CacheAllPokemon(client, basePath));
+            tasks.Add(PokemonFetch.CacheAllTypes(client, basePath));
             Task.WhenAll(tasks);
         }
     }
